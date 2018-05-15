7cb7bf85c677ab009d7972b1a7e29cef369876ab
myfailemtions <myfailemtions@gmail.com>
2018-05-15 21:10:07 +0800

    Export general component

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import VueCroppie from 'vue-croppie'
+import Imager from './components/Imager'
 
 Vue.use(VueCroppie)
 Vue.config.productionTip = false
 
 export {
-  VueCroppie
+  VueCroppie,
+  Imager
 }
